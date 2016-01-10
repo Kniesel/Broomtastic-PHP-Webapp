aa959826f907d7030e5512a8475d185317fc5fb6
gitTARDIS <anja.bergmann@hotmail.com>
2016-01-10 13:55:49 +0100

    changed sql script

--- database/products.sql
+++ database/products.sql
@@ -1,4 +1,6 @@
-CREATE TABLE IF NOT EXISTS products (
+DROP TABLE IF EXISTS products;
+
+CREATE TABLE products (
 	pk_productid SERIAL PRIMARY KEY,
 	productname VARCHAR(256) NOT NULL,
 	category VARCHAR(256) NOT NULL,
@@ -7,21 +9,21 @@ CREATE TABLE IF NOT EXISTS products (
 );
 
 INSERT INTO products (productname, category, price) VALUES ("Snitch", "Balls", 100);
-INSERT INTO products (productname, category, price) VALUES ("Quaffle", "Balls", 100);
-INSERT INTO products (productname, category, price) VALUES ("How to Quidditch", "Books", 100);
-INSERT INTO products (productname, category, price) VALUES ("Flying for dummies", "Books", 100);
-INSERT INTO products (productname, category, price, description) VALUES ("Black Gloves", "Clothing", 100, "Made of high-quality dragon leather");
-INSERT INTO products (productname, category, price) VALUES ("Black Gloves", "Clothing", 100);
-INSERT INTO products (productname, category, price) VALUES ("Golden Jacket", "Clothing", 100);
-INSERT INTO products (productname, category, price) VALUES ("Red Jacket", "Clothing", 100);
-INSERT INTO products (productname, category, price) VALUES ("Broomtastic 5000", "Brooms", 100);
-INSERT INTO products (productname, category, price) VALUES ("Snitch", "Balls", 100);
-INSERT INTO products (productname, category, price) VALUES ("Snitch", "Balls", 100);
-INSERT INTO products (productname, category, price) VALUES ("Snitch", "Balls", 100);
-INSERT INTO products (productname, category, price) VALUES ("Snitch", "Balls", 100);
-INSERT INTO products (productname, category, price) VALUES ("Snitch", "Balls", 100);
-INSERT INTO products (productname, category, price) VALUES ("Snitch", "Balls", 100);
-INSERT INTO products (productname, category, price) VALUES ("Snitch", "Balls", 100);
-INSERT INTO products (productname, category, price) VALUES ("Snitch", "Balls", 100);
-INSERT INTO products (productname, category, price) VALUES ("Snitch", "Balls", 100);
+INSERT INTO products (productname, category, price) VALUES ("Quaffle", "Balls", 50);
+INSERT INTO products (productname, category, price) VALUES ("Bludger", "Balls", 70);
+INSERT INTO products (productname, category, price) VALUES ("Bludger (Set of two)", "Balls", 135);
+INSERT INTO products (productname, category, price) VALUES ("How to Quidditch", "Books", 42);
+INSERT INTO products (productname, category, price) VALUES ("Flying for dummies", "Books", 18);
+INSERT INTO products (productname, category, price) VALUES ("Quidditch in a nutshell", "Books", 120);
+INSERT INTO products (productname, category, price) VALUES ("Quidditch for dummies", "Books", 18);
+INSERT INTO products (productname, category, price) VALUES ("Quidditch through the ages", "Books", 20);
+INSERT INTO products (productname, category, price, description) VALUES ("Black Gloves", "Clothing", 420, "Made of high-quality dragon leather");
+INSERT INTO products (productname, category, price) VALUES ("Shoes", "Clothing", 130);
+INSERT INTO products (productname, category, price) VALUES ("Black Gloves", "Clothing", 170);
+INSERT INTO products (productname, category, price) VALUES ("Golden Jacket", "Clothing", 90);
+INSERT INTO products (productname, category, price) VALUES ("Red Jacket", "Clothing", 90);
+INSERT INTO products (productname, category, price) VALUES ("Broomtastic 5000", "Brooms", 5000);
+INSERT INTO products (productname, category, price) VALUES ("Broomtastic Kids", "Brooms", 900);
+INSERT INTO products (productname, category, price) VALUES ("Broomtastic Senior", "Brooms", 900);
+INSERT INTO products (productname, category, price) VALUES ("Broomtastic Traveller", "Brooms", 1400);
 

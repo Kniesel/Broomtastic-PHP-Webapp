e98c873994e76cb0f020fdeac972bfc687c4b043
gitTARDIS <anja.bergmann@hotmail.com>
2016-01-10 16:15:26 +0100

    products.php

---
+++ business/products.php
@@ -0,0 +1,47 @@
+<?php
+// we now in presentation layer
+// we will include business layer to load business logic
+include("business.php");
+
+// init User Model from Business Logic
+$product = new Product();
+
+// load all products
+$data = $product->getAll();
+
+// prepare HTML Table
+function getHTMLTable($tabledata) {
+  $html = '<table id="producttable">';
+  $html .= '<thead><tr>';
+  $html .= '<th>Product</th>';
+  $html .= '<th>Category</th>';
+  $html .= '<th>Price</th>';
+  $html .= '<th>Description</th>';
+  $html .= '</tr></thead>';
+
+  foreach($tabledata as $product) {
+    $html .= '<tbody><tr>';
+    $html .= '<td>' . $user['productname'] . '</td>';
+    $html .= '<td>' . $user['category'] . '</td>';
+    $html .= '<td>' . $user['price'] . '</td>';
+    $html .= '<td>' . $user['description'] . '</td>';
+    $html .= '</tr></tbody>';
+  }
+
+  $html .= '</table>';
+
+  return $html;
+}
+
+// now we can output the requested data
+?>
+<html>
+ <head> 
+   <title>Broomtastic</title>
+ </head>
+ <body>
+ 
+ <?php echo getHTMLTable($data); ?><br />
+ 
+ </body>
+</html>

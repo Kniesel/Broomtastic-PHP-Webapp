1d7a5ffd13b0d74b4eacc994cf788287eca5694f
NewtScamander <elisabeth_0623@outlook.com>
2016-01-10 15:25:58 +0100

    filtermenü bearbeitet

--- business/business.php
+++ business/business.php
@@ -1,57 +1,32 @@
 <?php
 
-	$pk_username = $_POST["pk_username"]; 
-	echo "You entered the name: " . $pk_username . "\n <br>";
+// Business Layer
 
-	$userpassword = $_POST["userpassword"]; 
-	echo "You entered the password: " . $userpassword . "\n <br>";
+// now include database layer
+include("../database/database.php");
 
-	$servername = "localhost";
-	$username = "kirchmai14";
-	$password = "9cIb3tdL";
-	$dbname = "kirchmai14";
+$categories = $_POST["categories"]; 
+echo "You entered all categories \n <br>";
+
+$category = $_POST["category"]; 
+echo "You entered category: " . $category . "\n <br>";
+
+// Product Model
+class Product {
+	private $productDAO = null;
 	
+	public function __construct() {
+		$this->productDAO = new ProductDAO();
+	}
+	public function getAll($categories) {
+		$data = $this->productDAO->readAll();
+		return $data;
+	}
+	public function readByCategory($category) {
+		$data = $this->productDAO->read($category);
+		return $data;
+	}
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
 }
-
-//check if no data in string - does not work like it should...
-// wenn ma nämlich jz a passwort eingibt, wird des zu 0 
-//und wenn ma keins eingibt, wirds zu NULL in der tabelle - sprich, es geht immer noch rein -.-
-if ($userpassword == "") {
-	$userpassword = "NULL";
-}else{
-	$userpassword = "'" + $userpassword + "'";
-}
-
-$sql = 
-"INSERT INTO users (pk_username, userpassword)
-VALUES ('$pk_username', '$userpassword')";
-
-//tests vo mir
-// "INSERT INTO users (pk_username, userpassword)
-// VALUES ('$pk_username', '" + $userpassword + "')";
-
-// "INSERT INTO users(" +
-// 	"pk_username," +
-// 	"userpassword" +
-// 	") VALUES (" + 
-// 		"'$pk_username'" + "," +
-// 		mysql_real_escape_string($userpassword) + 
-// 		")";
-
-
-if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully <br>";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
-
-$conn->close();
 
 ?>
